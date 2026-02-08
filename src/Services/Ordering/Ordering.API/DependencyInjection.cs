9e8cf0efe4eeb22c1b0c5331f6c50fddd4106f21
Davutcsmc <davutcsmc@gmail.com>
2026-02-08 09:23:02 +0300

    Order.API layer, minimal API, Carter and REPR Pattern

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -30,7 +30,6 @@ namespace Ordering.API
                 {
                     ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
                 });
-
             return app;
         }
 
